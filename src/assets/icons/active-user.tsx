bd978b087f4ddcb71e392fe0ced7ec7da1fa4697
Davidintoaec <david@intoaec.com>
2023-09-20 19:47:00 +0530

    [Davidjayan] Adds icons

---
+++ src/assets/icons/active-user.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const ActiveIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    
+<svg xmlns="http://www.w3.org/2000/svg" {...props} viewBox="0 0 12 13" >
+<mask id="mask0_1250_174806"  maskUnits="userSpaceOnUse" x="0" y="0" width="12" height="13">
+<rect y="0.0742188" width="12" height="12" fill="url(#pattern0)"/>
+</mask>
+<g mask="url(#mask0_1250_174806)">
+<rect y="0.0742188" width="12" height="12" fill="#3CA2FF"/>
+</g>
+<defs>
+<pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+<use  transform="scale(0.0078125)"/>
+</pattern>
+<image id="image0_1250_174806" width="128" height="128" />
+</defs>
+</svg>
+);
+
+export default ActiveIcon;
+
+
+
+
+
+
+
+
